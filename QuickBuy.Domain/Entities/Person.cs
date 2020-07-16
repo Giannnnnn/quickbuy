2a3f803b97b644f1584b22e3b7feb1e5f80fbb22
Giovani Florek <39080661+Giannnnnn@users.noreply.github.com>
2020-07-17 05:45:03 +0800

    updates

---
+++ QuickBuy.Domain/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Domain.Entities
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
